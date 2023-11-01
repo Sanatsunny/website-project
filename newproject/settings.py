6936a44e1211c98e8c2d885c4fb5ec5c04af1ee8
Sanatsunny <149580444+Sanatsunny@users.noreply.github.com>
2023-11-01 16:29:25 +0530

    Update settings.py

--- newproject/settings.py
+++ newproject/settings.py
@@ -123,6 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='devikathampi20@gmail.com'
+EMAIL_HOST_PASSWORD='cyzwjoyygcazryut'
+EMAIL_USE_TLS=True
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'STATIC')
